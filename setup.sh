ee0038dd9ce5fe1d8c783a3ef8493d7ef8a81735
post-l <ludovic.post@epitech.eu>
2013-09-17 19:57:57 +0200

    Change bad package name for pip and handle .emacs.d if directory already exist

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syu zsh emacs python ipython pip nodejs rlwrap
+sudo pacman -Syu zsh emacs python ipython python-pip nodejs rlwrap
 sudo pip install virtualenv flake8
 sudo npm install -g jshint
 
@@ -8,19 +8,21 @@ chsh -s /usr/bin/zsh
 
 sudo cp clean /usr/bin/
 
+if [ -d ~/.emacs.d ]; then
+    mv ~/.emacs.d ~/.emacs.d~
+fi
+
 ln -sb "$PWD/.gitconfig" ~
 ln -sb "$PWD/.emacs" ~
 ln -sf "$PWD/.emacs.d" ~
 ln -sb "$PWD/.zshenv" ~
 ln -sb "$PWD/.zshrc" ~
 
-instruction="
-Now, open emacs and do:
+echo "Now, open emacs and do:
 
 M-x el-get-install RET jedi
 M-x package-install RET flycheck RET
 
 If you want zsh-syntax-highlighting:
-yaourt -S zsh-syntax-highlighting"
 
-echo "$instruction"
+yaourt -S zsh-syntax-highlighting"
